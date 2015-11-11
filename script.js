3687c37199e5dab2c704774c144564415f78fe3a
Kris Hermann <krishermann@gmail.com>
2015-11-11 16:54:23 -0500

    Lab13

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$.get("https://www.reddit.com/r/aww/.json", function(response){
+		
+		var posts = response.data.children;
+		/*console.log(post.author);
+		console.log(post.thumbnail);
+		console.log(post.title);
+		console.log(post.url);
+		console.log(post.created_UTC);*/
+
+
+		$.each(posts, function(index, post){
+		var $container =  $('<div />').appendTo($('body'))
+   		$('<img />').attr('src', post.data.thumbnail).appendTo($container);
+   		var $title = $('<a />').html('<h2>' +post.data.title + '</h2>' ).appendTo($container);
+   		$title.attr('href', post.data.url);
+   		
+		$('<p />').text(post.data.author).appendTo($container);
+
+		var postDate = new Date(post.data.created * 1000);
+
+		$('<p />').text(postDate).appendTo($container);
+
+		});
+	
+	var $img = $("img");
+	$img.on('click', function(){
+		$(this).toggleClass('image-size');
+	});
+
+	});
+
+});
